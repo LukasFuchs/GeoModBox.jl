7dbac2ce5d9090d95a8689f192730597d434b88b
tduretz <duretz@uni-frankfurt.de>
2024-07-17 12:53:05 +0200

    add Backward Euler for 2D diffusion

--- examples/HeatEquation/BackwardEuler.jl
+++ examples/HeatEquation/BackwardEuler.jl
@@ -1,6 +1,5 @@
 using GeoModBox.HeatEquation
-using ExactFieldSolutions
-using Plots
+using ExactFieldSolutions, LinearAlgebra, Plots, Printf
 
 function HeatEquation()
     # Spatial domain
@@ -14,14 +13,19 @@ function HeatEquation()
     x    = (c=LinRange(xlim.min-Δ.x/2, xlim.max+Δ.x/2, nc.x), v=LinRange(xlim.min-Δ.x, xlim.max+Δ.x, nv.x))
     y    = (c=LinRange(ylim.min-Δ.y/2, ylim.max+Δ.y/2, nc.y), v=LinRange(ylim.min-Δ.y, ylim.max+Δ.y, nv.y))
     # Time domain
-    nt   = 1
-    Δt   = 1e-3
+    nt   = 500
     t    = 0.
+    nout = 10 
+    # Iterations
+    niter = 10
+    ϵ     = 1e-10
     # Primitive variables
+    T_ex  = zeros(nc.x+2, nc.y+2)
     T     = zeros(nc...)
     T0    = zeros(nc...)
     Te    = zeros(nc...)
     # Derived fields
+    ∂T    = (∂x=zeros(nv.x, nc.x), ∂y=zeros(nc.x, nv.x))
     q     = (x=zeros(nv.x, nc.x), y=zeros(nc.x, nv.x))
     # Material parameters
     ρ     = zeros(nc...)
@@ -31,36 +35,53 @@ function HeatEquation()
     R     = zeros(nc...)
     # Boundary conditions
     BC   = (
-        type = (W=:Neumann, E=:Neumann, S=:Neumann, N=:Neumann),
+        type = (W=:Dirichlet, E=:Dirichlet, S=:Dirichlet, N=:Dirichlet),
+        # type = (W=:Neumann, E=:Neumann, S=:Neumann, N=:Neumann),
         val  = (W=zeros(nc.y), E=zeros(nc.y), S=zeros(nc.x), N=zeros(nc.x)))
-    # Exact solution on cell centroids
-    for i=1:nc.x, j=1:nc.y
-        X = [x.c[i], y.c[j], t]
-        s = Diffusion2D_Gaussian( X )
-        Te[i,j] = s.u
+    # Numbering 
+    Num    = (T=reshape(1:nc.x*nc.y, nc.x, nc.y),)
+    # Initial conditions
+    AnalyticalSolution!(T, x.c, y.c, t)
+    @. k.x = 1e-6 
+    @. k.y = 1e-6
+    @. ρ   = 1.0
+    @. Cp  = 1.0
+    Δt = max(Δ...)^2/(maximum(k.x)/minimum(ρ)/minimum(Cp))/4.1
+    # Time integration Loop
+    for it=1:nt
+        @printf("Time step = %05d\n", it)
+        t += Δt
+        @. T0 = T
+        # Exact solution on cell centroids
+        AnalyticalSolution!(Te, x.c, y.c, t)
+        # Exact solution on cell boundaries
+        BoundaryConditions!(BC, x.c, y.c, t)
+        # Iteration loop
+        for iter=1:niter
+            # Evaluate residual
+            ComputeResiduals!(R, T, T_ex, T0, ∂T, q, ρ, Cp, k, BC, Δ, Δt)
+            @printf("||R|| = %1.4e\n", norm(R)/length(R))
+            norm(R)/length(R) < ϵ ? break : nothing
+            # Assemble linear system
+            K  = AssembleMatrix(ρ, Cp, k, BC, Num, nc, Δ, Δt)
+            # Solve for temperature correction: Cholesky factorisation
+            Kc = cholesky(K.cscmatrix)
+            # Solve for temperature correction: Back substitutions
+            δT = -(Kc\R[:])
+            # Update temperature
+            @. T += δT[Num.T]
+        end
+        # Visualisation
+        if mod(it, nout)==0
+            p1 = plot(aspect_ratio=1, xlims=(xlim...,), ylims=(ylim...,))
+            p1 = heatmap!(x.c, y.c, Te', title="Analytics")
+            p2 = plot(aspect_ratio=1, xlims=(xlim...,), ylims=(ylim...,))
+            p2 = heatmap!(x.c, y.c, T', title="Numerics")
+            p3 = plot(aspect_ratio=1, xlims=(xlim...,), ylims=(ylim...,))
+            p3 = heatmap!(x.c, y.c, (abs.(T-Te))', title="Error")
+            display(plot(p1, p2, p3, layout=(2,2)))
+        end
     end
-    # Exact solution on cell boundaries
-    for j=1:nc.y
-        X = [xlim.min, y.c[j], t]
-        s = Diffusion2D_Gaussian( X )
-        BC.val.W[j] = s.u
-        X = [xlim.max, y.c[j], t]
-        s = Diffusion2D_Gaussian( X )
-        BC.val.E[j] = s.u
-    end
-    for i=1:nc.x
-        X = [x.c[i], ylim.min, t]
-        s = Diffusion2D_Gaussian( X )
-        BC.val.S[i] = s.u
-        X = [x.c[i], ylim.max, t]
-        s = Diffusion2D_Gaussian( X )
-        BC.val.N[i] = s.u
-    end
-    # Evaluate residual
-    ComputeResiduals!(R, T, T0, ρ, Cp, k, BC, Δ, Δt)
-    # Visualisation
-    p1 = plot(aspect_ratio=1, xlims=(xlim...,), ylims=(ylim...,))
-    p1 = heatmap!(x.c, y.c, Te')
 end
 
 HeatEquation()
--- src/HeatEquation/BackwardEuler.jl
+++ src/HeatEquation/BackwardEuler.jl
@@ -1,7 +1,61 @@
-@views function ComputeResiduals!(R, T, T0, ρ, Cp, k, BC, Δ, Δt)
-    R .= 1
+using ExtendableSparse
+
+function ComputeResiduals!(R, T, T_ex, T0, ∂T, q, ρ, Cp, k, BC, Δ, Δt)
+    @. T_ex[2:end-1,2:end-1] = T 
+    @. T_ex[  1,2:end-1] = (BC.type.W==:Dirichlet) * (2*BC.val.W - T_ex[    2,2:end-1]) + (BC.type.W==:Neumann) * (T_ex[    2,2:end-1] - Δ.x/k.x[  1,:]*BC.val.W)
+    @. T_ex[end,2:end-1] = (BC.type.E==:Dirichlet) * (2*BC.val.E - T_ex[end-1,2:end-1]) + (BC.type.E==:Neumann) * (T_ex[end-1,2:end-1] + Δ.x/k.x[end,:]*BC.val.E)
+    @. T_ex[2:end-1,  1] = (BC.type.S==:Dirichlet) * (2*BC.val.S - T_ex[2:end-1,    2]) + (BC.type.S==:Neumann) * (T_ex[2:end-1,    2] - Δ.y/k.y[:,  1]*BC.val.S)
+    @. T_ex[2:end-1,end] = (BC.type.N==:Dirichlet) * (2*BC.val.N - T_ex[2:end-1,end-1]) + (BC.type.N==:Neumann) * (T_ex[2:end-1,end-1] - Δ.y/k.y[:,end]*BC.val.N)
+    @. ∂T.∂x = (T_ex[2:end,2:end-1] - T_ex[1:end-1,2:end-1])/Δ.x
+    @. ∂T.∂y = (T_ex[2:end-1,2:end] - T_ex[2:end-1,1:end-1])/Δ.y
+    @. q.x   = -k.x * ∂T.∂x
+    @. q.y   = -k.y * ∂T.∂y
+    @. R     = ρ*Cp*(T - T0)/Δt + (q.x[2:end,:] - q.x[1:end-1,:])/Δ.x + (q.y[:,2:end] - q.y[:,1:end-1])/Δ.y  
 end
 
-@views function AssembleMatrix(R, T, T0, ρ, Cp, k, BC, Δ, Δt)
-    return K = 1
+function AssembleMatrix(rho, cp, k, BC, Num, nc, Δ, Δt)
+    # Linear system of equation
+    ndof   = maximum(Num.T)
+    K      = ExtendableSparseMatrix(ndof, ndof)
+    dx, dy = Δ.x, Δ.y
+    #############################
+    #       Heat equation       #
+    #############################
+    for i=1:nc.x, j=1:nc.y
+        # Equation number
+        ii = Num.T[i,j]
+        # Stencil
+        iS = ii - nc.x
+        iW = ii - 1
+        iC = ii
+        iE = ii + 1
+        iN = ii + nc.x
+        # Boundaries
+        inW    =  i==1    ? false  : true   
+        DirW   = (i==1    && BC.type.W==:Dirichlet) ? 1. : 0.
+        NeuW   = (i==1    && BC.type.W==:Neumann  ) ? 1. : 0.
+        inE    =  i==nc.x ? false  : true   
+        DirE   = (i==nc.x && BC.type.E==:Dirichlet) ? 1. : 0.
+        NeuE   = (i==nc.x && BC.type.E==:Neumann  ) ? 1. : 0.
+        inS    =  j==1    ? false  : true  
+        DirS   = (j==1    && BC.type.S==:Dirichlet) ? 1. : 0.
+        NeuS   = (j==1    && BC.type.S==:Neumann  ) ? 1. : 0.
+        inN    =  j==nc.y ? false  : true   
+        DirN   = (j==nc.y && BC.type.N==:Dirichlet) ? 1. : 0.
+        NeuN   = (j==nc.y && BC.type.N==:Neumann  ) ? 1. : 0.
+        # Material coefficient
+        kW = k.x[i,j]
+        kE = k.x[i+1,j]
+        kS = k.y[i,j]
+        kN = k.y[i,j+1]
+        ρ  = rho[i,j]
+        Cp = cp[i,j]
+        # Linear system coefficients
+        if inS K[ii,iS] = kS .* (DirS + NeuS - 1) ./ dy .^ 2 end
+        if inW K[ii,iW] = kW .* (DirW + NeuW - 1) ./ dx .^ 2 end
+        K[ii,iC] = Cp .* ρ ./ Δt + (-kN .* (-DirN + NeuN - 1) ./ dy + kS .* (DirS - NeuS + 1) ./ dy) ./ dy + (-kE .* (-DirE + NeuE - 1) ./ dx + kW .* (DirW - NeuW + 1) ./ dx) ./ dx
+        if inE K[ii,iE] = -kE .* (-DirE - NeuE + 1) ./ dx .^ 2 end
+        if inN K[ii,iN] = -kN .* (-DirN - NeuN + 1) ./ dy .^ 2 end
+    end
+    return flush!(K)
 end
