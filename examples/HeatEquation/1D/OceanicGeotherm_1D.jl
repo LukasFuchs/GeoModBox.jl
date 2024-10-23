a783d460cec3e195f70d18fe6604e957dbb922a2
Lukas Fuchs <fuchs@geophysik.uni-frankfurt.de>
2024-10-23 15:27:28 +0200

    Added 1D Oceanic Geotherm example

--- examples/HeatEquation/1D/OceanicGeotherm_1D.jl
+++ examples/HeatEquation/1D/OceanicGeotherm_1D.jl
@@ -1,5 +1,7 @@
-using 
-function OceanicGeotherm_1D(T,M,N,Py,t,plotparam=0)
+using Plots, SpecialFunctions
+using GeoModBox.HeatEquation.OneD
+
+function OceanicGeotherm_1D(T,M,N,Py,t,plotparam)
 # Function to calculate the 1D geotherm for an oceanic lithosphere.       #
 # Temperature is calculated by solving the 1-D heat equation assuming     #
 # variable thermal parameters and a radiogenic heat source.               #
@@ -39,187 +41,188 @@ function OceanicGeotherm_1D(T,M,N,Py,t,plotparam=0)
 #    LF - 22.10.2024 - juila                                              #
 # ======================================================================= #
     
-if isempty(M) && isempty(N) && isempty(Py) && isempty(t)
+if isempty(T) && isempty(M) && isempty(N) && isempty(Py) && isempty(t)
     ## ================================================================== #
     # Use some default values if no input parameters are defined ======== #
     # Constants --------------------------------------------------------- #
-    M   =   (
-        H       =   -200e3,             # Hight of the model [ m ]
-    )
-    NC  =   (
-        y       =   200,                # Number of grid points
-    )
-    Δ   =   (
-        y       =   M.H/NC.y,            # Grid resolution
-    )
-    yc          =   LinRange(-M.H + Δ.y/2.0,0.0 - Δ.y/2.0,NC.y)     # Depth [ m ]
+    H           =   200e3               #   Hight of the model [ m ]
+    nc          =   200                 #   Number of central grid points    
+    Δy          =   H/nc                #   Grid resolution
+    # Depth [ m ] ---
+    yc          =   LinRange(-H + Δy/2.0,0.0 - Δy/2.0,nc)     
+    yv          =   LinRange(-H,0,nc+1)
     
     Py  =   (
-        ρm      =   3000,               # Density [ kg/m^3 ]
-        cpm     =   1000,               # Heat capacity [ J/kg/K ]
-        km      =   3.0,                # Conductivity [ W/m/K ]
-        HM      =   0,                  # Heat generation rate [W/kg]; Q = ρ*H0
+        ρm      =   3000,               #   Density [ kg/m^3 ]
+        cpm     =   1000,               #   Heat capacity [ J/kg/K ]
+        km      =   3.0,                #   Conductivity [ W/m/K ]
+        HM      =   0,                  #   Heat generation rate [W/kg]; Q = ρ*H0
     )    
+    # ------------------------------------------------------------------- #
     # Initial Condition ------------------------------------------------- #
     T   =   (
-        Tpot    =   1315 + 273.15,      # Potential temperautre [ K ]
-        ΔTadi   =   0.5,                # Adiabatic temperature gradient [ K/km ]
-        Ttop    =   273.15              # Surface temperature [ K ]
+        Tpot    =   1315 + 273.15,      #   Potential temperautre [ K ]
+        ΔTadi   =   0.5,                #   Adiabatic temperature gradient [ K/km ]
+        Ttop    =   273.15,             #   Surface temperature [ K ]
+        T_ex    =   zeros(nc+2,1),    
     )
     T1  =   (
-        Tbot    =   T.Tpot + T.ΔTadi*abs(M.H/1e3),    # Bottom temperature [ K ]
+        Tbot    =   T.Tpot + T.ΔTadi*abs(H/1e3),    # Bottom temperature [ K ]
         T       =   T.Tpot .+ abs.(yc./1e3).*T.ΔTadi,   # Initial T-profile [ K ]
     )
     T   =   merge(T,T1)
       
-    Tini        =   T.T
-
+    Tini        =   zeros(nc,1)
+    Tini        .=   T.T
+    # ------------------------------------------------------------------- #
     # Boundary conditions ----------------------------------------------- #
     BC      =   (
         type    = (N=:Dirichlet, S=:Dirichlet),
-        val     = (N=:T.Ttop,S=:T.Tbot)
+        val     = (N=T.Ttop[1],S=T.Tbot[1])
     )
-    #T.utbf      =   -0.03;          # c     =   -k/q -> 90 mW/m^2
-    #T.ltbf      =   -0.0033;        # c     =   -k/q -> 10 mW/m^2
-    # ------------------------------------------------------------------- #    
+    # S      =   -0.03;          # c     =   -k/q -> 90 mW/m^2
+    # N      =   -0.0033;        # c     =   -k/q -> 10 mW/m^2
+    # ------------------------------------------------------------------- #
     # Time stability criterion ------------------------------------------ #
-    t       =   (
-        fac     =   0.8,                #   Courant criterion
-        max     =   60,                 #   Lithosphere age [ Ma ]
-        sca     =   60*60*24*365.25,    #   Seconds per year
-    )
-    t1      =   (
-        age     =   t.max*1e6*t.sca,    #   Age in seconds
-    )
-    t   =   merge(t,t1)
-    
-    # =================================================================== #    
-    
-    ## Plot Initial condition ------------------------------------------- #
+    fac     =   0.8                 #   Courant criterion
+    tmax    =   60                  #   Lithosphere age [ Ma ]
+    tsca    =   60*60*24*365.25     #   Seconds per year
+
+    age     =   tmax*1e6*tsca        #   Age in seconds    
+    # =================================================================== #        
+    # Plot Initial condition -------------------------------------------- #
     plotparam   =   1
-    #fig = figure;
-    #clf
-    #subplot(1,2,1)
-    #plot(T.T,M.z./1e3,'LineWidth',2)
-    #xlabel('$$T\ [K]$$','Interpreter','latex')
-    #ylabel('$$Depth\ [km]$$','Interpreter','latex')
-    #title('$$T-profile$$','Interpreter','latex')
-    #set(gca,'FontWeight','Bold','LineWidth',2,'FontSize',15,...
-    #    'TickLabelInterpreter','latex')
+    p = plot(Tini.-T.Ttop,yc./1e3, 
+        label="", 
+        xlabel="T [°C]", ylabel="z [m]", 
+        title="Initial Temperature",
+        xlim=(0,T.Tbot-T.Ttop),ylim=(-H/1e3,0))
+    display(p)
     # =================================================================== #
 else
     # =================================================================== #
-    # Initial Condition ------------------------------------------------- #    
+    # Initial Condition ------------------------------------------------- #
     T1  =   (
-        Tbot    =   T.Tpot + T.ΔTadi*abs(M.H./1e3),    # Bottom temperature [ K ]
+        Tbot    =   T.Tpot + T.ΔTadi*abs(H./1e3),    # Bottom temperature [ K ]
         T       =   T.Tpot .+ abs(yc./1e3).*T.ΔTadi,   # Initial T-profile [ K ]
     )
     T   =   merge(T,T1)
-      
-    Tini        =   T.T
+    Tini        =   zeros(nc,1)
+    Tini        .=   T.T
     # =================================================================== #
-    #if plotparam
-    #    fig = figure;
-    #    clf
-    #    subplot(1,2,1)
-    #    plot(T.T,M.z./1e3,'LineWidth',2)
-    #    xlabel('$$T\ [K]$$','Interpreter','latex')
-    #    ylabel('$$Depth\ [km]$$','Interpreter','latex')
-    #    title('$$T-profile$$','Interpreter','latex')
-    #    set(gca,'FontWeight','Bold','LineWidth',2,'FontSize',15,...
-    #        'TickLabelInterpreter','latex')
-    #end
+    if plotparam==1
+        p = plot(T.T.-T.Ttop,yc./1e3, 
+        label="", 
+        xlabel="T [°C]", ylabel="z [m]", 
+        title="Initial Temperature",
+        xlim=(0,T.Tbot-T.Ttop),ylim=(-H/1e3,0))
+        display(p)
+    end
 end
-# Setup Fields ========================================================= #
+# Setup Fields ========================================================== #
 Py1     =   (
-    ρ       =   Py.ρm.*ones(NC.y,1),
-    cp      =   Py.cpm.*ones(NC.y,1),
-    k       =   Py.km.*ones(NC.y,1),
-    H       =   Py.HM.*one(NC.y,1)
+    ρ       =   Py.ρm.*ones(nc,1),
+    cp      =   Py.cpm.*ones(nc,1),
+    k       =   Py.km.*ones(nc+1,1),
+    H       =   Py.HM.*ones(nc,1)
 )
 Py  =   merge(Py,Py1)
 Py2     =   (
-    κ       =  maximum(Py.k)/minimum(Py.rho)/minimum(Py.cp)     # Therma  l diffusivity [ m^2/s ] 
+    # Thermal diffusivity [ m^2/s ] 
+    κ       =  maximum(Py.k)/minimum(Py.ρ)/minimum(Py.cp),     
 )
 Py  =   merge(Py,Py2)
 T2  =   (
-    q   =   zeros(NC.y,1)
+    q   =   zeros(nc+1,1),
 )
 T   =   merge(T,T2)
 # ======================================================================= #
-## Time stability criterion ============================================= #
-t2  =   (
-    Δexp    =   N.dz^2/2/Py.kappa;  #   Stability criterion for explicit
-)
-t   =   merge(t,t2)
-t3  =   (
-    Δ   =   t.fac*t.Δexp,
-    nit =   ceil(t.age./t.dt),      #   Number of iterations
-)
-t   =   merge(t,t3)
-t4  =   (
-    time    =   zeros(1,t.nit),     #   Time array
-)
-t   =   merge(t,t4)
+# Time stability criterion ==0=========================================== #
+Δtexp   =   Δy^2/2/Py.κ             #   Stability criterion for explicit
+Δt      =   fac*Δtexp               #   total time step
+
+nit     =   ceil(Int64,age/Δt)      #   Number of iterations    
+
+time    =   zeros(1,nit)            #   Time array
 # ======================================================================= #
 # Calculate 1-D temperature profile ===================================== #
-for i = 1:t.nit
+#count   =   1
+for i = 1:nit
     if i > 1
-        t.time[i]   =   t.time[i-1] + t.dt
+        time[i]     =   time[i-1] + Δt
+    elseif time[i] > age
+        Δt          =   age - time[i-1]
+        time[i]     =   time[i-1] + Δt
     end
-    println(t.time)
-    #     [T]     =   SolveDiff1Dimplicit_vary(N,T,Py,t);
-    # [T]     =   SolveDiff1Dexplicit_vary(N,T,Py,t);
+    ForwardEuler1D!(T,Py,Δt,Δy,nc,BC)
+    #if i == nit || abs(time[i]/1e6/tsca - count*5.0) < Δt/1e6/tsca        
+    #    println(string("i = ",i,", time = ", time[i]/1e6/tsca))
+    #    #println(abs(time[i]/1e6/tsca - count*5.0))                
+    #    p = plot(T.T.-T.Ttop,yc./1e3, 
+    #        label=string("t = ",ceil(time[i]/1e6/tsca),"[Ma]"), 
+    #        xlabel="T [°C]", ylabel="z [m]", 
+    #        title="Initial Temperature",
+    #        xlim=(0,T.Tbot-T.Ttop),ylim=(-H/1e3,0))
+    #    display(p)
+    #    count = count + 1
+    #end
 end
 # ======================================================================= #
 # Calculate heaf flow =================================================== #
-#if size(Py.k,1)==1
-#    for j=1:NC.y
-#        T.q[j] = -Py.k * 
-#            (T.T[j+1] - T.T[j])/N.dz
-#    end
-#    T.q[1]          =   - Py.k*(T.T[2]-T.T[1])/Δ.y
-#    T.q[NC.y]       =   - Py.k*(T.T[NC.y]-T.T[N.nz-1])/Δ.y
-#else
-    #for j=1:NC.y
-    #    T.q(j) = -(Py.k(j+1) + Py.k(j))/2 * 
-    #        (T.T(j+1) - T.T(j))/N.dz;
-    #end
-    #T.q(1)          =   - Py.k(1)*(T.T(2)-T.T(1))/N.dz;
-    #T.q(N.nz-1)     =   - Py.k(N.nz)*(T.T(N.nz)-T.T(N.nz-1))/N.dz;
-#end
+# South ---
+T.T_ex[2:end-1]     =   T.T
+T.T_ex[1]   =   (BC.type.S==:Dirichlet) * (2 * BC.val.S - T.T_ex[2]) + 
+                (BC.type.S==:Neumann) * (T.T_ex[2] - BC.val.S*Δy)
+# North ---
+T.T_ex[end] =   (BC.type.N==:Dirichlet) * (2 * BC.val.N - T.T_ex[nc+1]) +
+                (BC.type.N==:Neumann) * (T.T_ex[nc+1] + BC.val.N*Δy)
+if size(Py.k,1)==1
+    for j=1:nc+1
+        T.q[j]  =   -Py.k * 
+            (T.T_ex[j+1] - T.T_ex[j])/Δy
+    end    
+else
+    for j=1:nc+1
+        T.q[j]  =   -Py.k[j] * 
+            (T.T_ex[j+1] - T.T_ex[j])/Δy
+    end
+end
+
 # ======================================================================= #
-    
-### Plot profile if requested ============================================ #
-#if plotparam
-#    if strcmp(T.lbound,'const') && strcmp(T.ubound,'const')
-#        T.Tana      =   zeros(N.nz,1);
-#        T.Tana      =   T.Tini + ...
-#            (T.T0 - T.Tpot).*erfc(-M.z./(2*sqrt(t.age*Py.kappa)));
-#        T.Tana(1)   =   T.T0;
-#    end
-#    figure(fig)
-#    subplot(1,2,1)
-#    hold on
-#    plot(T.T,M.z./1e3,'r-','LineWidth',2)
-#    if strcmp(T.lbound,'const') && strcmp(T.ubound,'const')
-#        plot(T.Tana,M.z/1e3,'--','LineWidth',2)
-#        legend('$$Initial$$',['$$T_{',num2str(t.age/1e6/t.tfac),'Ma}$$'],...
-#            '$$T_{HSCM}$$','Location','SouthWest','Interpreter','latex')
-#    else
-#        legend('$$Initial$$',['$$T_{',num2str(t.age/1e6/t.tfac),'Ma}$$'],...
-#            'Location','SouthWest','Interpreter','latex')
-#    end
-#    xlabel('$$T\ [ K ]$$','Interpreter','latex')
-#    ylabel('$$Depth\ [ km ]$$','Interpreter','latex')
-#    subplot(1,2,2)
-#    plot(T.q.*1e3,M.zc./1e3,'LineWidth',2)
-#    xlabel('$$q\ [ mW ]$$','Interpreter','latex')
-#    ylabel('$$Depth\ [ km ]$$','Interpreter','latex')
-#    title('$$Heat\ flux$$','Interpreter','latex')
-#    set(gca,'FontWeight','Bold','LineWidth',2,'FontSize',15,...
-#            'TickLabelInterpreter','latex')
-#end
+## Plot profile if requested ============================================ #
+if plotparam == 1
+    if BC.type.N==:Dirichlet && BC.type.S==:Dirichlet
+        Tana    =   zeros(nc,1)
+        Tana    .=   Tini .+ (T.Ttop - T.Tpot).*erfc.(-yc./(2*sqrt(age*Py.κ)))
+        Tana[end] =   T.Ttop
+    end    
+    q = plot(T.T.-T.Ttop,yc./1e3, 
+            label=string("t = ",ceil(maximum(time)/1e6/tsca),"[Ma]"), 
+            xlabel="T [°C]", ylabel="z [m]",
+            title="Initial Temperature",
+            xlim=(0,T.Tbot-T.Ttop),ylim=(-H/1e3,0),
+            layout=(1,2),subplot=1)        
+    if BC.type.N==:Dirichlet && BC.type.S==:Dirichlet
+        plot!(q,Tana.-T.Ttop,yc./1e3, 
+                label="T_HSCM",linestyle=:dash,
+                layout=(1,2),subplot=1)        
+    end        
+    q = plot!(T.q.*1e3,yv./1e3, 
+            label="", 
+            xlabel="q [ mW ]", ylabel="z [m]", 
+            title="Heat Flux",
+            ylim=(-H/1e3,0),
+            subplot=2)        
+    display(q)
+end
 # ======================================================================= #
 # keyboard
 end
+T   = []
+M   = []
+N   = [] 
+Py  = []
+t   = []
+plotparam = 1
+
+OceanicGeotherm_1D(T,M,N,Py,t,plotparam)
