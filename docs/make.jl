c5a14f12b7bffabe7d0802d9772f35cec634508c
Lukas Fuchs <lukas_fuchs84@outlook.com>
2025-04-15 21:19:38 +0200

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,10 @@ makedocs(
     pages = [
         "Home" => "index.md",
         "Governing Equation" => Any[
-            # "Solution" => "man/GESolution.md",
+            # "Solution" => Any[
+            #    "General" => "man/GESolution.md",
+            #    "Initial Condition" => "man/Ini.md",
+            # ],
             "Heat Diffusion Equation" => Any[
                 "General" => "man/DiffMain.md",
                 "1D" => "man/DiffOneD.md",
