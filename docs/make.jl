e05ffb5ae30c69cf149936ece593b551b299ffab
Lukas Fuchs <lukas_fuchs84@outlook.com>
2025-09-08 15:34:44 +0200

    add further english exercises

--- docs/make.jl
+++ docs/make.jl
@@ -40,10 +40,10 @@ makedocs(
             "02 - 1D Heat Diffusion (explicit)" => "man/exercises/02_1D_Heat_explicit.md", # Checked! 
             "03 - 1D Heat Diffusion (implicit)" => "man/exercises/03_1D_Heat_implicit.md", # Checked!
             "04 - 2D Heat Diffusion (stationary)" => "man/exercises/04_2D_Diffusion_Stationary.md", # Checked!
-            # "05 - 2D Heat Diffusion (Plume)" => "man/exercises/05_2D_Diffusion_TD_Plume.md", # Checked! 
-            # "05 - 2D Heat Diffusion (Sill)" => "man/exercises/05_2D_Diffusion_TD_Sill.md", # Checked!
-            # "06 - 1D Advection" => "man/exercises/06_1D_Advection.md", # Checked!
-            # "07 - 2D Energy Conservation" => "man/exercises/07_2D_Energy_Equation.md", # Checked!
+            "05 - 2D Heat Diffusion (Plume)" => "man/exercises/05_2D_Diffusion_TD_Plume.md", # Checked! 
+            "05 - 2D Heat Diffusion (Sill)" => "man/exercises/05_2D_Diffusion_TD_Sill.md", # Checked!
+            "06 - 1D Advection" => "man/exercises/06_1D_Advection.md", # Checked!
+            "07 - 2D Energy Conservation" => "man/exercises/07_2D_Energy_Equation.md", # Checked!
             # "08 - 1D Stokes" => "man/exercises/08_1D_Stokes.md",
             # "09 - 2D Falling Block (steady state)" => "man/exercises/09_2D_Falling_Block.md",
             # "10 - 2D Falling Block (time-dep)" => "man/exercises/10_2D_Falling_Block_td.md",
